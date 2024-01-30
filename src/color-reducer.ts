947c117c75215209d04b0e194b280847d5e2da48
Angel Ivanov <angel.iwanov@gmail.com>
2024-01-30 22:01:51 +0200

    rgb

--- src/color-reducer.ts
+++ src/color-reducer.ts
@@ -1,19 +1,5 @@
 import { rgb } from 'color-convert';
 
-type UpdateHexColorAction = {
-  type: 'update-hex-color';
-  payload: {
-    hexColor: string;
-  };
-};
-
-type UpdateRGBColorAction = {
-  type: 'update-rgb-color';
-  payload: {
-    rgb: [number, number, number];
-  };
-};
-
 type ColorState = {
   hexColor: string;
 };
@@ -24,14 +10,14 @@ export const initialState: ColorState = {
 
 export const colorReducer = (
   state: ColorState = initialState,
-  action: UpdateHexColorAction | UpdateRGBColorAction,
+  action: AdjustColorActions,
 ) => {
   if (action.type === 'update-hex-color') {
     const { hexColor } = action.payload;
     return { ...state, hexColor };
   }
   if (action.type === 'update-rgb-color') {
-    const hexColor = rgb.hex(action.payload.rgb);
+    const hexColor = '#' + rgb.hex(action.payload.rgb);
     return { ...state, hexColor };
   }
   return state;
---
+++ src/global.d.ts
@@ -0,0 +1,15 @@
+type UpdateHexColorAction = {
+  type: 'update-hex-color';
+  payload: {
+    hexColor: string;
+  };
+};
+
+type UpdateRGBColorAction = {
+  type: 'update-rgb-color';
+  payload: {
+    rgb: [number, number, number];
+  };
+};
+
+type AdjustColorActions = UpdateHexColorAction | UpdateRGBColorAction;
